3a2101fb8c5660a9ff8fc71e4e8208ee51971b32
Gabig0l <gabigol1002@outlook.com>
2026-04-03 02:08:51 +0100

    feat: add drinks to bar, center beer pong card to fit image

--- server.py
+++ server.py
@@ -355,6 +355,20 @@ def seed_database(conn: sqlite3.Connection) -> None:
         (15,"Box de Kebab","Menu da Noite",7.00,"available",24),
         (16,"Box Batata + Molho","Menu da Noite",3.00,"available",25),
         (17,"Fatia de Pizza","Menu da Noite",1.50,"available",26),
+        (18,"Fino","Bebidas com Álcool",1.00,"available",30),
+        (19,"Balde Fino","Bebidas com Álcool",2.50,"available",31),
+        (20,"Metro 11 Finos","Bebidas com Álcool",10.00,"available",32),
+        (21,"Metro 15 Finos","Bebidas com Álcool",13.00,"available",33),
+        (22,"Cadeira 20 Finos","Bebidas com Álcool",18.00,"available",34),
+        (23,"Vinho Copo Branco / Tinto","Bebidas com Álcool",1.00,"available",35),
+        (24,"Vinho Garrafa Branco / Tinto","Bebidas com Álcool",4.00,"available",36),
+        (25,"Favaios Simples","Bebidas com Álcool",1.00,"available",37),
+        (26,"Favaios C/ Cerveja","Bebidas com Álcool",1.50,"available",38),
+        (27,"Shot Mariquinhas","Bebidas com Álcool",0.50,"available",39),
+        (28,"Água 0,50L","Bebidas sem Álcool",1.00,"available",40),
+        (29,"Coca Cola","Bebidas sem Álcool",1.50,"available",41),
+        (30,"Ice Tea","Bebidas sem Álcool",1.50,"available",42),
+        (31,"Bebida Energética","Bebidas sem Álcool",2.50,"available",43),
     ]
     conn.executemany("INSERT INTO bar_products (id,name,category,price,availability,sort_order) VALUES (?,?,?,?,?,?)", bar_products)
 
